cf58feb3641460288c7a3e27224b7cf4fcc78dc2
Valeska Fabris <valeskafabris@Mayks-MacBook-Pro.local>
2024-01-25 03:42:04 -0300

    add "Home" page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import { Container } from "./style";
+
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { Rating } from "../../components/Rating";
+import { Tag } from "../../components/Tag";
+
+import { FiPlus } from "react-icons/fi";
+
+export function Home() {
+  return(
+    <Container>
+
+    </Container>
+  );
+}
