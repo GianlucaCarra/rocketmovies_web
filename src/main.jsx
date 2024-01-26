78256d138f31a84b4265f6b0e6f9c32b4c82ebb4
Valeska Fabris <valeskafabris@Mayks-MacBook-Pro.local>
2024-01-25 21:35:47 -0300

    SignUp page add

--- src/main.jsx
+++ src/main.jsx
@@ -11,12 +11,13 @@ import { Home } from './pages/Home';
 import { Profile } from './pages/Profile';
 
 import { SignIn } from './pages/SignIn';
+import { SignUp } from './pages/SignUp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>,
 );
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -4,25 +4,26 @@ import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { ButtonText } from "../../components/ButtonText";
 
-import { FiMail, FiLock } from "react-icons/fi";
+import { FiUser, FiMail, FiLock, FiArrowLeft} from "react-icons/fi";
 
-export function SignIn(){
+export function SignUp(){
   return(
     <Container>
       <Form>
         <h1>RocketMovies</h1>
         <span>Application to keep track of everything you watch.</span>
 
-        <h2>Make your login</h2>
+        <h2>Make your account</h2>
 
         <div>
+          <Input icon={FiUser} placeholder="Username" type="text" />
           <Input icon={FiMail} placeholder="E-mail" type="email" />
           <Input icon={FiLock} placeholder="Password" type="password" />
         </div>
         
-        <Button title="Sign in" />
+        <Button title="Sign up" />
 
-        <ButtonText title="Create Acount" />
+        <ButtonText icon={FiArrowLeft} title="Return to Log In" />
       </Form>
 
       <Background />
